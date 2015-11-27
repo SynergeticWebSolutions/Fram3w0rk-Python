2e57e929db19ebd864680d4616eb1bba595f1e57
Jonathan Lawton <jlawton2@users.noreply.github.com>
2015-11-27 02:27:55 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'fram3w0rk-python',
+  packages = ['fram3w0rk-python'],
+  version = '0.5',
+  description = '"Class" effort to unify functions across 30 languages.',
+  author = 'Jonathan Lawton',
+  author_email = 'jlawton@lawtonsoft.com',
+  url = 'https://github.com/LawtonSoft/Fram3w0rk-Python',
+  download_url = 'https://github.com/LawtonSoft/Fram3work-Python/tarball/0.1',
+  keywords = ['fram3w0rk', 'mvc', 'web'],
+  classifiers = [],
+)
